f59c3c9b1571a30f823d0d2c1ebbf58adf123f88
William <William@Crosky>
2017-11-05 02:59:10 +0100

    Add start/end date main

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,11 +1,15 @@
 package helloworld;
 
+import java.util.Date;
+
 public class HelloWorld
 {
 
 	public static void main(String[] args)
 	{
+		System.out.println(new Date());
 		System.out.println("Hello World!");
+		System.out.println(new Date());
 	}
 
 }
